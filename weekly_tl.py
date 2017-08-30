1968ed30799c56c8cef5d4e74c3e4a0d84e10196
Dariusz Gadomski <dariusz.gadomski@canonical.com>
2017-08-30 16:59:51 +0200

    [weekly_tl] Prettier output formatting and simple formatting for -n.

--- weekly_tl.py
+++ weekly_tl.py
@@ -5,6 +5,8 @@ import argparse
 from os.path import expanduser
 from gtimelog.timelog import TimeWindow, format_duration_short, as_minutes
 
+branch = '├──'
+branch_last = '└──'
 gt_file = '%s/.local/share/gtimelog/timelog.txt' % expanduser("~")
 virtual_midnight = datetime.time(2, 0)
 userid = 'dgadomski'
@@ -17,6 +19,23 @@ def get_time():
     week_last = week_first + datetime.timedelta(days=4)
     week_last = week_last.replace(hour=23, minute=59, second=59)
     return (week_first, week_last)
+
+
+def lookahead(iterable):
+    """Pass through all values from the given iterable, augmented by the
+    information if there are more values to come after the current one
+    (True), or if it is the last value (False).
+    """
+    # Get an iterator and pull the first value.
+    it = iter(iterable)
+    last = next(it)
+    # Run the iterator to exhaustion (starting from the second value).
+    for val in it:
+        # Report the *previous* value (more to come).
+        yield last, True
+        last = val
+    # Report the last value.
+    yield last, False
 
 
 def main():
@@ -45,28 +64,29 @@ def main():
         else:
             categories = sorted(entries)
         for cat in categories:
-            print('%s:' % cat)
+            print('%s:' % cat.strip())
 
             work = [(entry, duration)
                     for start, entry, duration in entries[cat]]
             work.sort()
-            for entry, duration in work:
+            for (entry, duration), has_more in lookahead(work):
                 if not duration:
                     continue  # skip empty "arrival" entries
 
                 entry = entry[:1].upper() + entry[1:]
                 if args.no_time:
-                    print(u"  %-61s  " % entry)
+                    print(u"%s" % entry)
                 else:
-                    print(u"  %-61s  %+5s %+4s" %
-                          (entry, format_duration_short(duration),
+                    print(u"%s %-62s%+5s %+4s" %
+                          (branch if has_more else branch_last,
+                           entry, format_duration_short(duration),
                            as_minutes(duration)))
 
             if args.no_time:
                 print("")
             else:
                 print('-' * 75)
-                print(u"%+70s %4s" % (format_duration_short(totals[cat]),
+                print(u"%+71s %4s" % (format_duration_short(totals[cat]),
                                       as_minutes(totals[cat])))
         print("Total work done : %s" % format_duration_short(total_work))
 if __name__ == '__main__':
